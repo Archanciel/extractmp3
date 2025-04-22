72702b21bcb824e70ed5abc35f56f1fc8d46c3f7
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-04-22 16:19:06 +0200

    set windows app size. Works.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
 import 'views/audio_extractor_view.dart';
 import 'viewmodels/audio_extractor_viewmodel.dart';
 
-void main() {
+Future<void> main() async {
+  await setWindowsAppSizeAndPosition();
+
   runApp(const MyApp());
+}
+
+/// If app runs on Windows, Linux or MacOS, set the app size
+/// and position.
+Future<void> setWindowsAppSizeAndPosition({
+  bool isTest = true,
+}) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await getScreenList().then((List<Screen> screens) {
+      // Assumez que vous voulez utiliser le premier écran (principal)
+      final Screen screen = screens.first;
+      final Rect screenRect = screen.visibleFrame;
+
+      // Définissez la largeur et la hauteur de votre fenêtre
+      double windowWidth = (isTest) ? 900 : 730;
+      const double windowHeight = 1300;
+
+      // Calculez la position X pour placer la fenêtre sur le côté droit de l'écran
+      final double posX = screenRect.right - windowWidth + 10;
+      // Optionnellement, ajustez la position Y selon vos préférences
+      final double posY = (screenRect.height - windowHeight) / 2;
+
+      final Rect windowRect =
+          Rect.fromLTWH(posX, posY, windowWidth, windowHeight);
+      setWindowFrame(windowRect);
+    });
+  }
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import file_picker
 import path_provider_foundation
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
