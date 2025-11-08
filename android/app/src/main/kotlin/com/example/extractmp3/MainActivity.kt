22d64af326c582b5e617894a6442c09487a0e804
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-11-08 17:58:20 +0100

    BINGO, now works on emulator. UN énorme merci à Toi, mon Dieu, même merci à Toi, Jésus !

--- android/app/src/main/kotlin/com/example/extractmp3/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/extractmp3/MainActivity.kt
@@ -1,22 +1,6 @@
+// android/app/src/main/kotlin/com/example/extractmp3/MainActivity.kt
 package com.example.extractmp3
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    private val CHANNEL_EXTRACTOR = "audio_extractor"
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL_EXTRACTOR)
-            .setMethodCallHandler { call, result ->
-                AudioExtractorNative.handle(
-                    call,
-                    result,
-                    applicationContext.contentResolver,
-                    cacheDir,
-                    getExternalFilesDir(null) ?: filesDir
-                )
-            }
-    }
-}
+class MainActivity : FlutterActivity()
